c89fd0e4e78ace696dddea15d9c351d55238bbdb
Michael Boro <michaelboro1122@gmail.com>
2019-10-24 20:20:29 -0400

    added env variables and mongoose

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const userRoutes = require('./routes/users');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use('/users', userRoutes);
 
