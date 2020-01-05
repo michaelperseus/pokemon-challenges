b0b3f1814da2bc89be48652725464f6d7bb14c9b
Michael Boro <michaelboro1122@gmail.com>
2020-01-04 20:46:08 -0500

    updated for heroku prod

--- server/services/file-upload.js
+++ server/services/file-upload.js
@@ -2,7 +2,7 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-const config = require('../../config/config');
+// const config = require('../../config/config');
 
 //Configures the AWS connection
 aws.config.update({
