eae80b25ee28add4644627773b8f6c43b4c384ae
Michael Boro <michaelboro1122@gmail.com>
2020-01-30 13:29:08 -0500

    bugfix testing

--- client/src/components/AddPokemon.js
+++ client/src/components/AddPokemon.js
@@ -33,6 +33,7 @@ export default class EditPokemon extends Component {
 
     handleSubmit = async (e) => {
         e.preventDefault();
+        alert('Adding....');
         const update = {
             pokemon: this.state.species,
             starter: this.state.starter,
@@ -43,7 +44,7 @@ export default class EditPokemon extends Component {
         await fetch(`https://pokeapi.co/api/v2/pokemon/${update.pokemon}/`)
         .then(async res => {
             if (res.status !== 200) {
-                return console.log('invalid pokemon')
+                return alert('invalid pokemon')
             }
             await fetch(`/runs/addPokemon/${this.props.match.params.runId}`, {
                 method: 'POST',
